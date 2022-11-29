bc57bde0f7e983bd282cb343f53e76c2082de642
md.nizamuddinshamrat <pridesys.0027@gmail.com>
2022-11-29 14:42:20 +0600

    splash screen designed

--- lib/app/modules/splash/views/splash_view.dart
+++ lib/app/modules/splash/views/splash_view.dart
@@ -1,3 +1,5 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:ecommerce_demo_flutter/app/config/app_assets.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -9,16 +11,54 @@ class SplashView extends GetView<SplashController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('SplashView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'SplashView is working',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+          child: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 126, 221, 255),
+              Color.fromARGB(255, 88, 64, 226),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
-      ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset(
+              AppAssets.ASSET_APP_LOGO,
+              height: 80,
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'E-Commerce',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                  color: Colors.white),
+            ),
+            const SizedBox(height: 10),
+            AnimatedTextKit(
+              animatedTexts: [
+                TypewriterAnimatedText(
+                  textAlign: TextAlign.center,
+                  'You can buy anything from here!',
+                  textStyle:
+                      const TextStyle(fontSize: 16.0, color: Colors.white),
+                  speed: const Duration(milliseconds: 50),
+                ),
+              ],
+              totalRepeatCount: 2,
+              pause: const Duration(milliseconds: 200),
+              displayFullTextOnTap: true,
+              stopPauseOnTap: true,
+            )
+          ],
+        ),
+      )),
     );
   }
 }
