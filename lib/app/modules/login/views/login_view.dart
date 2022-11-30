4f51e1c140b19ed9b99c6310e5ff39816c2142a4
md.nizamuddinshamrat <pridesys.0027@gmail.com>
2022-11-30 12:31:28 +0600

    login page designed

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,3 +1,6 @@
+import 'package:ecommerce_demo_flutter/app/components/custom_text_field.dart';
+import 'package:ecommerce_demo_flutter/app/components/primary_button.dart';
+import 'package:ecommerce_demo_flutter/app/config/app_assets.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -9,16 +12,36 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('LoginView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'LoginView is working',
-          style: TextStyle(fontSize: 20),
+        body: SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              AppAssets.ASSET_APP_LOGO,
+              height: 80,
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'You can buy anything from here.',
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 40),
+            const CustomTextField(
+                label: 'Email', validationText: 'Enter your email'),
+            const SizedBox(height: 10),
+            const CustomTextField(
+                label: 'Password', validationText: 'Enter your Password'),
+            const SizedBox(height: 20),
+            PrimaryButton(
+              onClick: () {},
+              text: 'LOGIN',
+            )
+          ],
         ),
       ),
-    );
+    ));
   }
 }
