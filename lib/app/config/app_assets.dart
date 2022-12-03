a3f27c2e70ca68b49d3151a5e896db90f5f8c30e
nizamuddinshamrat <nshamrat7@gmail.com>
2022-12-04 03:15:54 +0600

    menu and empty location added

--- lib/app/config/app_assets.dart
+++ lib/app/config/app_assets.dart
@@ -2,4 +2,6 @@ class AppAssets {
   static const String ASSET_APP_LOGO = 'assets/logo/ecommerce.png';
   static const String ASSET_BOTTLE_ICON = 'assets/icon/bottle.png';
   static const String ASSET_BRAND_ICON = 'assets/icon/brand.png';
+  static const String ASSET_EMPTY_ICON = 'assets/icon/empty.png';
+  static const String ASSET_PRODUCT_ICON = 'assets/icon/product.png';
 }
