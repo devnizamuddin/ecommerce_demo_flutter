fbf3208d761aaba784042ccda11a7b1b1b441327
nizamuddinshamrat <nshamrat7@gmail.com>
2022-12-04 02:28:48 +0600

    bottle and brand image location added

--- lib/app/config/app_assets.dart
+++ lib/app/config/app_assets.dart
@@ -1,3 +1,5 @@
 class AppAssets {
   static const String ASSET_APP_LOGO = 'assets/logo/ecommerce.png';
+  static const String ASSET_BOTTLE_ICON = 'assets/icon/bottle.png';
+  static const String ASSET_BRAND_ICON = 'assets/icon/brand.png';
 }
