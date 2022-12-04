cb74002ed9f314bd74475eb91f4f8e24e6a42719
md.nizamuddinshamrat <pridesys.0027@gmail.com>
2022-12-04 13:48:26 +0600

    shape unused code removed

--- lib/app/components/login_top_shape.dart
+++ lib/app/components/login_top_shape.dart
@@ -5,7 +5,13 @@ import 'package:flutter/material.dart';
 Widget LoginTopBar() {
   return CustomPaint(
     painter: MyCustomPainter(),
-    child: Container(height: 160),
+    child: Container(
+      width: double.maxFinite,
+      height: 160,
+      child: const Padding(
+        padding: EdgeInsets.only(top: 32.0),
+      ),
+    ),
   );
 }
 
@@ -30,41 +36,6 @@ class MyCustomPainter extends CustomPainter {
         size.width * 0.6988889,
         size.height * 0.7580000);
     path0.quadraticBezierTo(size.width * 0.9794444, size.height * 0.7630000,
-        size.width * 0.9977778, size.height * 0.9940000);
-    path0.lineTo(size.width, size.height * 0.0020000);
-    path0.lineTo(size.width * 0.0011111, size.height * 0.0020000);
-    path0.close();
-
-    canvas.drawPath(path0, paint0);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
-  }
-}
-
-class RPSCustomPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    Paint paint0 = Paint()
-      ..color = const Color.fromARGB(255, 33, 150, 243)
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 1;
-
-    Path path0 = Path();
-    path0.moveTo(size.width * 0.0011111, size.height * 0.0020000);
-    path0.lineTo(0, size.height * 0.8740000);
-    path0.quadraticBezierTo(size.width * 0.1588889, size.height * 0.6630000,
-        size.width * 0.2455556, size.height * 0.6620000);
-    path0.cubicTo(
-        size.width * 0.3530556,
-        size.height * 0.6670000,
-        size.width * 0.3613889,
-        size.height * 0.8530000,
-        size.width * 0.6988889,
-        size.height * 0.7580000);
-    path0.quadraticBezierTo(size.width * 0.9627778, size.height * 0.7530000,
         size.width * 0.9977778, size.height * 0.9940000);
     path0.lineTo(size.width, size.height * 0.0020000);
     path0.lineTo(size.width * 0.0011111, size.height * 0.0020000);
