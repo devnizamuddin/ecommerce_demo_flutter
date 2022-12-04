e8fb36dcba3ec8934458530e0764d052b456064d
md.nizamuddinshamrat <pridesys.0027@gmail.com>
2022-12-04 11:38:48 +0600

    bottom navigation routing updated

--- lib/app/modules/index/views/index_view.dart
+++ lib/app/modules/index/views/index_view.dart
@@ -1,5 +1,7 @@
+import 'package:ecommerce_demo_flutter/app/modules/account/views/account_view.dart';
 import 'package:ecommerce_demo_flutter/app/modules/cart/views/cart_view.dart';
 import 'package:ecommerce_demo_flutter/app/modules/home/views/home_view.dart';
+import 'package:ecommerce_demo_flutter/app/modules/services/views/services_view.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -51,8 +53,12 @@ class IndexView extends GetView<IndexController> {
     switch (controller.currentIndex.value) {
       case 0:
         return const HomeView();
+      case 1:
+        return const ServicesView();
       case 2:
         return const CartView();
+      case 3:
+        return const AccountView();
       default:
         return const HomeView();
     }
